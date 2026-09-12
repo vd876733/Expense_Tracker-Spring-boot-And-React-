bae904c2fa1f06186e5eac71ac0c9977e17ab0cc
vd876733 <vd876733@gmail.com>
2026-09-12 11:40:54 +0530

    Fix Lombok annotations on RecurringTransaction entity

--- src/main/java/com/financetracker/entity/RecurringTransaction.java
+++ src/main/java/com/financetracker/entity/RecurringTransaction.java
@@ -1,12 +1,16 @@
 package com.financetracker.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "recurring_transactions")
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class RecurringTransaction {
 
@@ -26,56 +30,7 @@ public class RecurringTransaction {
     @Column(nullable = false)
     private Integer dayOfMonth;
 
+    @Builder.Default
     @Column(nullable = false)
     private Boolean active = true;
-
-    // --- MANUALLY ADDED GETTERS & SETTERS TO FIX COMPILATION ERRORS ---
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Integer getDayOfMonth() {
-        return dayOfMonth;
-    }
-
-    public void setDayOfMonth(Integer dayOfMonth) {
-        this.dayOfMonth = dayOfMonth;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
 }
