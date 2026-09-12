788aa52cf865a93eb5c40f55682d2b5a72e508aa
vd876733 <vd876733@gmail.com>
2026-09-12 15:36:13 +0530

    Update WebConfig CORS to allow Vercel origin and all route paths

--- src/main/java/com/financetracker/controller/AuthController.java
+++ src/main/java/com/financetracker/controller/AuthController.java
@@ -189,7 +189,7 @@ public class AuthController {
             com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier verifier = 
                 new com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier.Builder(
                     new com.google.api.client.http.javanet.NetHttpTransport(), 
-                    com.google.api.client.json.jackson2.JacksonFactory.getDefaultInstance())
+                    com.google.api.client.json.gson.GsonFactory.getDefaultInstance())
                 .setAudience(java.util.Collections.singletonList(googleClientId))
                 .build();
                 
